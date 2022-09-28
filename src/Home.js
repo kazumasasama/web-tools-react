e8b399a8d2f8d219e6983ff3a7463835afcc7480
Kaz <takaik@yahoo.com>
2022-09-28 14:57:57 -0400

    delete Message, fix bug/ DictEng

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,17 @@
 import * as React from 'react'
-import Message from './components/Message';
+import { Row, Col, Card } from 'react-bootstrap';
 
 function Home() {
   return(
-    <div>
-      <h1>HOME</h1>
-      <Message
-        message="What's up?"
-      />
-    </div>
+    <Row>
+      <Col>
+        <Card>
+          <Card.Body>
+            <Card.Title><h1>Welcome to Web Tools</h1></Card.Title>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
   )
 }
 
