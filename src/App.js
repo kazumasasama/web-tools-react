20f1bba55d0f882ddae6f3fb7719a63fd2ea7101
kt4 <kaz.nyc.dev@gmail.com>
2022-10-07 21:16:37 -0400

    ?

--- src/App.js
+++ src/App.js
@@ -1,41 +1,29 @@
 import * as React from 'react'
-import { Container } from 'react-bootstrap';
-import { Routes, Route, useLocation } from "react-router-dom";
+import {Container, Row, Col} from 'react-bootstrap';
 import Home from './Home';
 import Header from './Header';
+import {Routes, Route} from "react-router-dom";
 import Counter from './Counter';
 import Timer from './Timer';
 import Calculator from './Calculator';
-import Generator from './Generator';
-import Dictionary from './Dictionary';
-import WorldClock from './WorldClock';
-import Text from './Text';
-import Sort from './Sort';
 
 export default function App() {
-
-  let header
-  const location = useLocation();
-  if (location.pathname !== '/') {
-    header = (
-      <Header/>
-    )
-  }
-
   return (
-    <Container fluid>
-      {header}
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/timer' element={<Timer />} />
-        <Route path='/counter' element={<Counter />} />
-        <Route path='/calculator' element={<Calculator />} />
-        <Route path='/generator' element={<Generator />} />
-        <Route path='/dictionary' element={<Dictionary />} />
-        <Route path='/world-clock' element={<WorldClock />} />
-        <Route path='/text' element={<Text />} />
-        <Route path='/sort' element={<Sort />} />
-      </Routes>
-    </Container>
+    <Container fluid id="base-container">
+      <Header/>
+        <Row>
+          <Col>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/timer' element={<Timer />} />
+              <Route path='/counter' element={<Counter />} />
+              <Route path='/calculator' element={<Calculator />} />
+              <Route path='/generator' element={<Generator />} />
+              <Route path='/dictionary' element={<Dictionary />} />
+              <Route path='/world-clock' element={<WorldClock />} />
+            </Routes>
+          </Col>
+        </Row>
+      </Container>
   );
 }
